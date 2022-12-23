41d7fa7b70cd7f41c6b66feaf82bcee356229e12
Sunny <sunnyrathaur2444@gmail.com>
2022-12-23 20:55:37 +0530

    ADD react ICON Package for UseEffectAPI project

--- src/App.js
+++ src/App.js
@@ -8,8 +8,8 @@ import './App.css';
 // import BasicForm from './components/Form/BasicForm';
 // import UseEffects1 from './components/useEffect/UseEffects1';
 // import UseEffect2 from './components/useEffect/UseEffect2';
-// import UseEffectAPI from './components/useEffect/UseEffectAPI';
-import Uncontrolled from './components/Form/Uncontrolled';
+import UseEffectAPI from './components/useEffect/UseEffectAPI';
+// import Uncontrolled from './components/Form/Uncontrolled';
 
 const App = () => {
   return (
@@ -39,11 +39,11 @@ const App = () => {
       {/* <UseEffect2/> */}
 
       {/* Github Users Data using useEffect Hook with Fetch API */}
-      {/* <UseEffectAPI/> */}
+      <UseEffectAPI/>
 
       {/* Ref & useRef Hooks | Controlled and Uncontrolled Form Components */}
-      <Uncontrolled/>
-      
+      {/* <Uncontrolled/> */}
+
     </div>
   )
 }
--- src/components/useEffect/apiData/UsersCard.js
+++ src/components/useEffect/apiData/UsersCard.js
@@ -1,4 +1,5 @@
 import React from 'react'
+import { FcApproval } from "react-icons/fc";
 
 const UsersCard = ({ users }) => {
   return (
@@ -17,7 +18,7 @@ const UsersCard = ({ users }) => {
                     <div className="d-flex align-items-center">
                       <div className="image"> <img src={avatar_url} className="rounded" width="155" /> </div>
                       <div className="ml-3 w-100">
-                        <h4 className="mb-0 mt-0 textLeft">{login}</h4>
+                        <h4 className="mb-0 mt-0 textLeft">{login}<FcApproval/></h4>
                         <span className="text-left">{type}</span>
                         <div className="p-2 mt-2 bg-primary d-flex justify-content-between rounded text-white stats">
                           <div className="d-flex flex-column">
